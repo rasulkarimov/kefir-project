cbd6fca29c51efa28eefbaa30cab8dc9f4255b49
rasul <rasul.karimov@gmail.com>
2021-03-04 22:40:02 +0000

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "kefir-service"
+  location = "europe-north1"
+  template {
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/minScale" = "1"
+        "autoscaling.knative.dev/maxScale" = "5"
+      }
+    }
+    spec {
+      container_concurrency = 80
+      containers {
+        image = "gcr.io/kefir-306607/kefir:latest"
+        ports {
+          container_port = 80
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "project" {
+  description = "name of the project to use"
+  default = "kefir-306607"
+}
+
+variable "region" {
+  description = "region for using"
+  default = "europe-north1"
+}
